4a1b4d86e8a1d5d3be12e3209f785ac5a5b9f587
Hana Harencarova <h.harencarova@gmail.com>
2020-09-17 03:34:25 +0200

    Add second counter

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -28,3 +28,30 @@ var x = setInterval(function() {
 }, 1000);
 
 
+var countDownDateSEO = new Date("Sept 30, 2020 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+
+  var distance = countDownDateSEO - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("countdownSEO").innerHTML =
+  days + "<span class='small-text'>days </span>"
+  + hours + "<span class='small-text'>h </span>"
+  + minutes + "<span class='small-text'>min </span>"
+  + seconds + "<span class='small-text'>s </span>";
+
+  // If the count down is over, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdownSEO").innerHTML = "REGISTRATION CLOSED";
+  }
+}, 1000);
+
