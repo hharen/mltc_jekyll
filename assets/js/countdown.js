f548026e9167940145793b7660c3364ca748dc07
Hana Harencarova <h.harencarova@gmail.com>
2020-09-19 14:10:55 +0200

    Add courses to the main index page

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,6 +1,6 @@
 // Set the date we're counting down to
 
-var countDownDate = new Date("Oct 12, 2020 00:00:00").getTime();
+var countDownDate = new Date("Sept 28, 2020 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -28,7 +28,7 @@ var x = setInterval(function() {
 }, 1000);
 
 
-var countDownDateSEO = new Date("Sept 30, 2020 00:00:00").getTime();
+var countDownDateSEO = new Date("Sept 28, 2020 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
